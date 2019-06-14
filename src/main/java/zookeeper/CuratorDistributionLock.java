557cd473c2649a519128c6a677ce006d7c018968
hhx胡宏星 <hongxinghu@ctrip.com>
2019-06-14 14:11:07 +0800

    zookeeper

---
+++ src/main/java/zookeeper/CuratorDistributionLock.java
@@ -0,0 +1,31 @@
+package zookeeper;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class CuratorDistributionLock {
+
+    public static void main(String[] args) {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy);
+        client.start();
+
+        InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock");
+        try {
+            mutex.acquire();
+            System.out.println("Enter mutex");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                mutex.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
